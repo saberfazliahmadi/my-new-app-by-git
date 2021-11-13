0ad46a85f6f6fb0aa70f490a594ebc5da687e069
saber fazliahmadi <saberfazliahmadi@gmail.com>
2021-11-13 17:12:41 +0330

    v 0.8

--- new file 1.php
+++ new file 1.php
@@ -1,13 +1,3 @@
 <?php
-<<<<<<< HEAD
-echo 'Hello this is me' ;
-=======
-echo "Hello this is SABER" ;
->>>>>>> f771113250b3b473c5061b7a59d3a364c627ed58
+echo 'hello' ;
 ?>
-<<<<<<< HEAD
-Hellooooooo
-=======
-
-saaaaaabeeeeer
->>>>>>> ec963201e20b808671bf5c91121595f66ea067f9
